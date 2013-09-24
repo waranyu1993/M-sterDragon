5811bb69437c51202b7de2e56718058cdf6437d3
Dragon <waranyu.1993@gmail.com>
2013-09-25 01:27:27 +0700

    Edit some output

--- CalculateDN/src/Project/Calulate.java
+++ CalculateDN/src/Project/Calulate.java
@@ -4,8 +4,6 @@ public class Calulate {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Dragon");
-		System.out.println("O___O");
-		System.out.println(".......................................");
+		System.out.println("That's Dragon");
 	}
 }
